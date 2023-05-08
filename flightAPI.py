02a0678561f2a3a78eec5b49568161f417b89bb6
ryanxjacques <58966393+ryanxjacques@users.noreply.github.com>
2023-05-08 12:13:06 -0700

    Accessing data!

--- flightAPI.py
+++ flightAPI.py
@@ -3,25 +3,36 @@ Interacting my flightAPI from https://aviationstack.com/
 """
 
 import requests
+import json
+
 
 url = "http://api.aviationstack.com/v1/flights"
 params = {
-    "access_key": "71b8a3f1273c8d7ce386086d7bef9f3b"
+    "access_key": "71b8a3f1273c8d7ce386086d7bef9f3b",
+    "flight_status": "active"
 }
 
-response = requests.get(url, params=params)
+api_result = requests.get(url, params=params)
 
 # check if the request was successful
-if response.status_code == 200:
+if api_result.status_code == 200:
     # extract the response data as a dictionary
-    data = response.json()
+    api_response = api_result.json()
 
-    # do something with the response data
-    print(data)
+    for flight in api_response['data']:
+    # for flight in api_response['data']:
+        live_data = flight.get('live')
+        if live_data and not live_data.get('is_ground', True):
+            print(u'%s flight %s from %s (%s) to %s (%s) is in the air.' % (
+                flight['airline']['name'],
+                flight['flight']['iata'],
+                flight['departure']['airport'],
+                flight['departure']['iata'],
+                flight['arrival']['airport'],
+                flight['arrival']['iata']))
 else:
-    print("Error:", response.status_code)
+    print(f"Error: {api_result.status_code} -- {api_result.reason}")
 
-print(data)
 
 
 
